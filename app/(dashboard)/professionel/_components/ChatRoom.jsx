5548b64245312f663f36711683c202aa3c9919a6
Stéphanie Maminiaina <stephanipageot42@gmail.com>
2024-11-19 11:41:07 +0100

    messagerie sans image okay

--- app/(dashboard)/professionel/_components/ChatRoom.jsx
+++ app/(dashboard)/professionel/_components/ChatRoom.jsx
@@ -1,128 +1,3 @@
-// "use client";
-// import React, { useEffect } from "react";
-// import MessageCard from "./MessageCard";
-// import MessageInput from "./MessageInput";
-
-// const ChatRoom = ({ user, selectedChatroom }) => {
-//   useEffect(() => {
-//     console.log("Selected chatroom in ChatRoom.jsx:", selectedChatroom);
-//   }, [selectedChatroom]);
-
-// const messages = [
-//   {
-//     id: 1,
-//     sender: "katty",
-//     avatarUrl:
-//       "https://res.cloudinary.com/dtryutlkz/image/upload/v1731399830/ixqce5t3fkrs7e3qstg2.jpg",
-//     content: "Hey test",
-//     time: "2h ago",
-//   },
-//   {
-//     id: 2,
-//     sender: "douglas",
-//     avatarUrl:
-//       "https://res.cloudinary.com/dtryutlkz/image/upload/v1731399830/ixqce5t3fkrs7e3qstg2.jpg",
-//     content: "Hey wassup",
-//     time: "2h ago",
-//   },
-// ];
-
-//   return (
-// <div className="flex flex-col h-screen">
-//   <div className="flex-1 overflow-y-auto p-10">
-//     {/* message card */}
-//     {messages?.map((message) => (
-//       <MessageCard key={message.id} message={message} user={"douglas"} />
-//     ))}
-//   </div>
-//   {/* message input */}
-//   <MessageInput />
-// </div>
-//   );
-// };
-
-// export default ChatRoom;
-// "use client";
-// import React, { useEffect, useState, useRef } from "react";
-// import MessageCard from "./MessageCard";
-// import MessageInput from "./MessageInput";
-// import { db } from "@/firebaseconfig";
-// import { ref, get, serverTimestamp, query } from "firebase/database";
-// import {
-//   addDoc,
-//   collection,
-//   doc,
-//   onSnapshot,
-//   orderBy,
-//   updateDoc,
-//   where,
-// } from "firebase/firestore";
-
-// const ChatRoom = ({ user, selectedChatroom }) => {
-//   const [message, setMessage] = useState("");
-//   const [messages, setMessages] = useState("");
-//   const messagesContainerRef = useRef(null);
-//   useEffect(() => {
-//     console.log("From ChatRoom, selectedChatroom:", selectedChatroom);
-//   }, [selectedChatroom]);
-
-//   const me = selectedChatroom?.myData;
-//   const other = selectedChatroom?.otherData;
-//   const chatRoomId = selectedChatroom?.id;
-
-//   if (!selectedChatroom) {
-//     return <div>Sélectionnez un chatroom pour afficher les détails.</div>;
-//   }
-//   const sendMessage = async (e) => {
-//     const messageCollection = collection(db, "messages");
-//     if (message.trim() === "") {
-//       return;
-//     }
-//     try {
-//       const messageData = {
-//         chatRoomId,
-//         content: message,
-//         image: "",
-//         messageType: "text",
-//         senderId: me.id,
-//         receiverId: other.id,
-//         timestamp: serverTimestamp(),
-//       };
-//       await addDoc(messageCollection, messageData);
-//       setMessage("");
-
-//       //maj with lastmessage
-//       const chatroomRef = doc(db, "chatrooms", chatRoomId);
-//       await updateDoc(chatroomRef, { lastMessage: messageData });
-//     } catch (error) {
-//       console.log("erreur:", error);
-//     }
-//   };
-//   return (
-//     <div>
-//       <h1>Chatroom Details</h1>
-//       <p>Chatroom ID: {selectedChatroom.id}</p>
-//       <p>Receiver Name: {selectedChatroom.otherData?.nom || "Inconnu"}</p>
-//       <p>Last Message: {selectedChatroom.latestMessage || "Aucun message"}</p>
-//       <div className="flex flex-col h-screen">
-//         <div className="flex-1 overflow-y-auto p-10">
-//           {/* message card */}
-//           {/* {messages?.map((message) => (
-//             <MessageCard key={message.id} message={message} user={"douglas"} />
-//           ))} */}
-//         </div>
-//         {/* message input */}
-//         <MessageInput
-//           sendMessage={sendMessage}
-//           message={message}
-//           setMessage={setMessage}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-// export default ChatRoom;
-
 "use client";
 import React, { useEffect, useState, useRef } from "react";
 import MessageCard from "./MessageCard";
@@ -136,10 +11,7 @@ const ChatRoom = ({ user, selectedChatroom }) => {
   const messagesContainerRef = useRef(null);
 
   useEffect(() => {
-   // console.log("From ChatRoom, selectedChatroom:", selectedChatroom);
-
     if (selectedChatroom) {
-      // Fetch messages for the current chatroom
       const messagesRef = ref(db, `messages/${selectedChatroom.id}`);
       const unsubscribe = onValue(messagesRef, (snapshot) => {
         if (snapshot.exists()) {
@@ -156,7 +28,6 @@ const ChatRoom = ({ user, selectedChatroom }) => {
         }
       });
 
-      // Clean up the listener when the component unmounts or chatroom changes
       return () => unsubscribe();
     }
   }, [selectedChatroom]);
@@ -164,6 +35,7 @@ const ChatRoom = ({ user, selectedChatroom }) => {
   const me = selectedChatroom?.myData;
   const other = selectedChatroom?.otherData;
   const chatRoomId = selectedChatroom?.id;
+  
 
   if (!selectedChatroom) {
     return <div>Sélectionnez un chatroom pour afficher les détails.</div>;
@@ -184,15 +56,13 @@ const ChatRoom = ({ user, selectedChatroom }) => {
         timestamp: serverTimestamp(),
       };
 
-      // Add the message to the database
       const messagesRef = ref(db, `messages/${chatRoomId}`);
       await push(messagesRef, messageData);
 
-      // Update the last message in the chatroom
       const chatroomRef = ref(db, `chatrooms/${chatRoomId}`);
       await update(chatroomRef, { lastMessage: message });
 
-      setMessage(""); // Clear the input field
+      setMessage("");
     } catch (error) {
       console.error("Erreur lors de l'envoi du message :", error);
     }
--- app/(dashboard)/professionel/_components/MessageInput.jsx
+++ app/(dashboard)/professionel/_components/MessageInput.jsx
@@ -5,9 +5,8 @@ import { FaPaperclip, FaPaperPlane } from "react-icons/fa";
 const MessageInput = ({ sendMessage, message, setMessage }) => {
   return (
     <div className="flex items-center p-4 border-t border-gray-200">
-      {/* attach files */}
       <FaPaperclip className="text-gray-700 mr-2 cursor-pointer" />
-      {/* input */}
+
       <input
         type="text"
         placeholder="type a message"
@@ -15,7 +14,7 @@ const MessageInput = ({ sendMessage, message, setMessage }) => {
         onChange={(e) => setMessage(e.target.value)}
         className="flex-1 border-none p-2 outline-none"
       />
-      {/* send message */}
+
       <FaPaperPlane
         onClick={() => {
           sendMessage();
